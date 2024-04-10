816beff6b631309c0688e642c1a988d6a289247f
Veronica Fontana <veronyc98@hotmail.it>
2024-04-10 12:49:53 +0200

    [add] Header logo

--- script.js
+++ script.js
@@ -20,12 +20,14 @@ loadTemplate("header", "#header");
 window.addEventListener("scroll", function(){
     const header = document.querySelector("#header-box");
     const logo = document.querySelector("logo");
+    const content = document.querySelector("#content");
     
     if (window.scrollY > 0) {
-        header.style.top = "-140px";
+        header.style.top = "-217px";
         logo.style.height = "0";
+        content.style.margin = "70px 0px 0px 0px";
     } else {
         header.style.top = "0";
-        logo.style.height = "140px"; 
+        logo.style.height = "217px"; 
     }
 });
